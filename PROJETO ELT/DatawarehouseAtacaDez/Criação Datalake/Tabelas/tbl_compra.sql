79c31549c38daadcd0618c2af62548e7fb4dc536
Fabricio Rodrigues <fabriciorodrigues1234@gmail.com>
2023-11-29 00:01:48 -0300

    Versão 02 do projeto do DataLake Ataca Dez

---
+++ PROJETO ELT/DatawarehouseAtacaDez/Criação Datalake/Tabelas/tbl_compra.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tbl_compra]
+(
+	[mes_ano] VARCHAR(25) NOT NULL , 
+    [cod_produto] VARCHAR(25) NOT NULL, 
+    [preco_compra] FLOAT NULL, 
+    [arquivo_origem] VARCHAR(200) NULL, 
+    [data_carga] DATETIME NULL, 
+    PRIMARY KEY ([cod_produto], [mes_ano])
+)
---
+++ PROJETO ELT/DatawarehouseAtacaDez/Criação Datalake/Tabelas/tbl_log.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[tbl_log]
+(
+	[data] INT NOT NULL PRIMARY KEY
+)
